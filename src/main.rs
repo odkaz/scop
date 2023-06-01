ab3ba69e9253b1553d23407509583a9f208606b6
odkaz <one.destiny.japan@gmail.com>
2023-06-01 20:58:41 +0200

    add: cam directioN

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,9 @@ fn main() {
     let mut initialFoV = 45.0;
 
     let speed = 3.0; // 3 units / second
-    let mouseSpeed = 0.005;
+    let mouseSpeed = 0.0005;
+    let mut prevMouseX = 0;
+    let mut prevMouseY = 0;
     let now = Instant::now();
     let mut elapsed = now.elapsed();
     let mut lastTime = elapsed.as_millis();
@@ -233,10 +235,12 @@ fn main() {
         println!("delta{}", deltaTime);
 
         lastTime = currentTime;
-        // horizontalAngle += mouseSpeed * deltaTime * (width / 2 - mouse.x() as u32) as f32;
-        // verticalAngle += mouseSpeed * deltaTime * (height / 2 - mouse.y() as u32) as f32;
-        horizontalAngle = mouse.x() as f32;
-        verticalAngle = mouse.y() as f32;
+        horizontalAngle += mouseSpeed * deltaTime * (prevMouseX - mouse.x()) as f32;
+        verticalAngle += mouseSpeed * deltaTime * (prevMouseY - mouse.y()) as f32;
+        prevMouseX = mouse.x();
+        prevMouseY = mouse.y();
+        // horizontalAngle = mouse.x() as f32;
+        // verticalAngle = mouse.y() as f32;
         let direction = glm::vec3(
             f32::cos(verticalAngle) * f32::sin(horizontalAngle),
             f32::sin(verticalAngle),
@@ -256,18 +260,19 @@ fn main() {
         );
         let trans = glm::scale(&trans, &glm::vec3(0.5, 0.5, 0.5)); //scale
 
-        // let width = 900;
-        // let height = 700;
         let Projection = glm::perspective(
             glm::radians(&glm::vec1(45.0))[0],
             width as f32 / height as f32,
             0.1,
             100.0,
         );
+        let cam_pos = glm::vec3(4.0, 3.0, 3.0);
+        let origin = glm::vec3(0.0, 0.0, 0.0);
         let View = glm::look_at(
-            &glm::vec3(4.0, 3.0, 3.0), // Camera is at (4,3,3), in World Space
-            &glm::vec3(0.0, 0.0, 0.0), // and looks at the origin
-            // &direction,
+            // &glm::vec3(4.0, 3.0, 3.0), // Camera is at (4,3,3), in World Space
+            &cam_pos,
+            &(cam_pos + direction), // and looks at the origin
+            // &origin,
             &glm::vec3(0.0, 1.0, 0.0), // Head is up (set to 0,-1,0 to look upside-down)
         );
         let Model = trans;
